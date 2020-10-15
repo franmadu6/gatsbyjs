146c29d4972b881302ea2502da69315f36fe28e1
franmadu6 <frandh1997@gmail.com>
2020-10-15 17:59:35 +0200

    configuraciones esteticas

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,25 +1,25 @@
 const config = {
-  siteTitle: 'Gatsby Markdown Blog Starter', // Site title.
-  siteTitleShort: 'Gatsby Markdown Blog Starter', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: 'Gatsby Markdown Blog Starter', // Alternative site title for SEO.
+  siteTitle: 'Fran Madueño Blog', // Site title.
+  siteTitleShort: 'Madueño Markdown Blog', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: 'Fran Madueño Blog', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
   siteUrl: 'https://github.com/franmadu6/gatsbyjs/', // Domain of your website without pathPrefix.
   pathPrefix: 'https://github.com/franmadu6/gatsbyjs/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription:
-    'A Gatsby Markdown Blog stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
+    'Web estatica generada por Gatsby.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: '1825356251115265', // FB Application ID for using app insights
   googleAnalyticsID: 'UA-161211056-1', // GA tracking ID.
   dateFromFormat: 'YYYY-MM-DD', // Date format used in the frontmatter.
   dateFormat: 'DD/MM/YYYY', // Date format for display.
-  userName: 'Jone Due', // Username to display in the author segment.
-  userEmail: 'youremail@gmail.com', // Email used for RSS feed's author segment
-  userTwitter: 'gatsbyjs', // Optionally renders "Follow Me" in the Bio segment.
-  userGitHub: 'gatsbyjs', // Optionally renders "Follow Me" in the Bio segment.
-  userLocation: 'North Pole, Earth', // User location to display in the author segment.
+  userName: 'Fran Madueño', // Username to display in the author segment.
+  userEmail: 'frandh1997@gmail.com', // Email used for RSS feed's author segment
+  userTwitter: 'franmadueo', // Optionally renders "Follow Me" in the Bio segment.
+  userGitHub: 'franmadu6', // Optionally renders "Follow Me" in the Bio segment.
+  userLocation: 'Sevilla, España', // User location to display in the author segment.
   userAvatar: 'https://i.ibb.co/WPz9CNk/avatar.jpg', // User avatar to display in the author segment.
   userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+    "Aquí hay una descripción", // User description to display in the author segment.
   copyright: 'Copyright © 2020. All rights reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#c62828', // Used for setting manifest and progress theme colors.
   backgroundColor: 'red' // Used for setting manifest background color.
