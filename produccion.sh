f78649216b710ae093ea7bb408743233fafe42b2
franmadu6 <frandh1997@gmail.com>
2020-10-17 18:22:13 +0200

    Publicación añadida por el Script de Producción

--- produccion.sh
+++ produccion.sh
@@ -4,12 +4,14 @@
 #y el despliegue automático de la página en el entorno de producción, después de realizar un cambio de la página en el entorno de desarrollo. 
 #Muestra al profesor un ejemplo de como al modificar la página se realiza la puesta en producción de forma automática. (3 puntos)
 
+echo '##################################-G A T S B Y J S-#############################################'
 echo 'Scrip de Automatización de ficheros, Ejecutelo en Administrador para no tener posibles fallos.'
 #año-mes-dia actuales:
 time=$(date +%Y-%m-%d)
-echo "Nombre de la publicación cambia los espacios por '-':"
+echo "Nombre de la publicación,(cambia los espacios por '-'):"
 read nom
 
+#Creo plantilla por defecto para rellenar
 echo "---
 date: $time
 title: "$nom"
@@ -26,9 +28,13 @@ tags:
 Comienza a escribir tu publicación
 " > content/$time-$nom.md
 
+#Escribo el contenido de mi publicación
 nano content/$time-$nom.md
 
-
+#Subir los archivos a Github
 sudo git add .
 sudo git commit -m "Publicación añadida por el Script de Producción"
 sudo git push
+
+#Cuando se ejecute npm run deploy todo el contenido de la public carpeta se moverá a la rama master de mi repositorio .
+npm run deploy
