a9b34a703bce85f878305c36c2a6e2a08399e669
franmadu6 <frandh1997@gmail.com>
2020-12-16 16:38:29 +0100

    Updates

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ self.__precacheManifest = [
     "url": "webpack-runtime-c0076ae1df6698f1eb05.js"
   },
   {
-    "url": "commons-ac5466880dc982a22a5f.js"
+    "url": "commons-2f87c35fe3c7432de957.js"
   },
   {
     "url": "app-3faf24a9353f23273b6b.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a3a48836c46e48fe81edc9e2006380b7"
+    "revision": "462e6b779d52d864c1b7a84d2110ef11"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -47,7 +47,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "26981608bfeae92a9dc06a0cd4e5f376"
+    "revision": "de1a9010f69942ddfa6afb703c1c5c0d"
   },
   {
     "url": "manifest.webmanifest",
